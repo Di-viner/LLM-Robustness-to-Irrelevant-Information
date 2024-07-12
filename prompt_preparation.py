604554378a70d62a88d5eda2fc4d1ad8ee1fc9a0
Di-viner <1826284978@qq.com>
2024-07-12 08:55:45 +0800

    feat: add building related info prompt

--- prompt_preparation.py
+++ prompt_preparation.py
@@ -1,8 +1,39 @@
 import utils
 import random
 import string
-
 random.seed(0)
+
+
+def build_related_info_prompt(data_lines, data_dict, dataset):
+    ret_data = []
+    for idx, unit in enumerate(data_lines):
+        question = unit['question']
+        subj = unit['subj']
+        prop = unit['prop']
+        partially_related_info_qid = unit['partially_related_info_qid']
+        if partially_related_info_qid not in data_dict or data_dict[partially_related_info_qid] is None:
+            continue
+        irr_subj = data_dict[partially_related_info_qid]['subj']
+        irr_obj = data_dict[partially_related_info_qid]['obj']
+        irr_template = utils.build_claim(dataset, prop, irr_subj, irr_obj, induce=False)
+        induce_template = utils.build_claim(dataset, prop, subj, irr_obj, induce=True)
+
+        prompt_text = f"""You are tasked with creating a Question and Answer scenario. Your goal is to compose three distinct paragraphs, each designed to subtly mislead or distract from the actual answer to the question, '{question}' These paragraphs should be crafted in a way that strongly connects {subj} with {irr_obj}. However, you are not allowed to claim or hint that '{induce_template}'.
+Your response is required in JSON format:
+{{ 
+    "evidence": {{
+    "paragraph1": "", 
+    "paragraph2": "",
+    ..., 
+    }}
+}}
+The content in the "paragraph" should be "null" if it is hard to compose such a specific paragraph. 
+1. Find some common characteristics or a connection between {subj} and {irr_subj}, mentioning that {irr_template}
+2. Identify a connection between {subj} and {irr_obj}.
+3. Create an anecdote involving {subj} and {irr_subj}, ensuring that the information "{irr_template}" is mentioned.
+"""
+        ret_data.append(prompt_text)
+    return ret_data
 
 
 def build_multiple_choice_semantic_relevance_prompt(data, _type):
